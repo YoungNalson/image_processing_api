9b3b9a210257bfc89dbd19683bc6b56adcab8a89
YoungNalson <caiopsilva01@gmail.com>
2024-08-21 16:47:14 -0300

    Iniciando a configuração do celery para tarefas em segundo plano da api

---
+++ api/celery_config.py
@@ -0,0 +1,19 @@
+from celery import Celery
+
+# Configuração do Celery
+celery_app = Celery(
+    'tasks',
+    broker='redis://localhost:6379/0',  # URL do broker Redis
+    backend='redis://localhost:6379/0',  # URL do backend Redis
+    include=['image_processing.process_images', 'tasks.api_tasks']
+)
+
+# Configurações adicionais, se necessário
+# celery_app.conf.update(
+#     task_routes={
+#         'myapp.tasks.add': 'low-priority',
+#     },
+#     timezone='UTC',
+# )
+
+# from image_processing.process_images import process_images
